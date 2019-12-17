29416c675101e5599ae5c1320350f49ef55b8c46
gcshift <gcshift@xinics.com>
2019-12-17 19:02:47 +0900

    EpisodeVotePolls랑 VoteElement 작업 진행중. git --amend로 이어받아요

--- src/components/EpisodeVotePolls/VoteElement/index.tsx
+++ src/components/EpisodeVotePolls/VoteElement/index.tsx
@@ -1,20 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Typography, TextField } from '@material-ui/core';
 import styles from './styles';
 import { EpisodeData } from '../../../entities/EpisodeData';
+import Vote from '../Vote';
 
 interface VoteElementProps {
+  index?: number;
   episodeData?: EpisodeData;
+  onChanged?: (index: number, vote: Vote) => void;
 }
 
-const VoteElement: FC<VoteElementProps> = ({ episodeData }) => {
+const VoteElement: FC<VoteElementProps> = ({ index, episodeData }) => {
   const classes = styles();
+  const [vote, setVote] = useState<Vote>({})
+
+  const handleEpisodeChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target as HTMLInputElement;
+    console.log(value);
+  };
+
+  const handleIndexChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target as HTMLInputElement;
+    console.log(value);
+  };
 
   return (
     <div className={classes.root}>
-      <TextField className={classes.textField} label="회차" />
+      <TextField className={classes.textField} label="회차" onChange={ handleEpisodeChanged } />
       <Typography variant="h6">회</Typography>
-      <TextField className={classes.textField} label="번호" />
+      <TextField className={classes.textField} label="번호" onChange={ handleIndexChanged } />
       <Typography variant="h6">번</Typography>
       { episodeData &&
         <Typography color="textSecondary" className={classes.textFieldDesc}>
