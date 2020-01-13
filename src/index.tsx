6d1c8c83b1df3a12a4e2ba6aabd51ef8f32b4a4f
gcshift <gcshift@xinics.com>
2020-01-13 16:03:30 +0900

    IE polyfill 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { polyfill } from 'es6-promise';
+
 import './root.scss';
+
+polyfill();
 
 ReactDOM.render(
   <App />,
