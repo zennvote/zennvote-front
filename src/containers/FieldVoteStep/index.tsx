bacee49bafbee67d8357d1bb7161489b5f979b03
gcshift <gcshift@xinics.com>
2020-01-14 16:57:07 +0900

    FieldVoteStep 안내 문구 수정

--- src/containers/FieldVoteStep/index.tsx
+++ src/containers/FieldVoteStep/index.tsx
@@ -24,6 +24,21 @@ const FieldVoteStep: FC<FieldVoteStepProps> = ({ onNextStep }) => {
         <li><Typography>큐에, 라일락, 류드, 처빕, 아토, 초코, 엔류, 모함과, 히키코모리, 태민 및 이 열명이 참여한 유닛</Typography></li>
         <li><Typography>오프닝</Typography></li>
       </ol>
+      <Typography variant="h6" className={classes.subtitle}>
+        <b>이것도 읽고 가라구!</b>
+      </Typography>
+      <ol>
+        <li><Typography>부문상에도 <b>유닛</b>을 뽑을 수 <b>있습니다.</b></Typography></li>
+        <li>
+          <Typography>
+            <b>
+              출연 회차와 곡이 달라도 동일한 프로듀서를 서로 다른 부문에 또 한번 더 투표할 수 없습니다.
+              <br />즉, 가창력 상에 처빕의 A회 X번을 뽑고 음색상에 처빕의 C회 N번을 또 적어 넣을 수 없어요.
+              <br />처빕은 5부문 합쳐서 딱 한번만 투표 가능한 것입니다.
+            </b>
+          </Typography>
+        </li>
+      </ol>
       <Divider className={classes.divider} />
       <PitchVoteCard />
       <div className={classes.actionRoot}>
