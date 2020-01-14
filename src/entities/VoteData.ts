271c09c025b88d3de6564120dd8c4e3cdcf42acd
BuildTools <unconfigured@null.spigotmc.org>
2020-01-14 08:56:36 +0000

    VoteDate 구조 수정
    - data가 2depth면 redux에서 초기화하기 귀찮아짐 ㅎㅎ

--- src/entities/VoteData.ts
+++ src/entities/VoteData.ts
@@ -1,24 +1,22 @@
 export interface VoteData {
     email?: string;
-    data?: {
-        problem?: number[];      //문제 10개
-        pitch?: string[];        //가창력 상
-        voice?: string[];        //음색 상
-        funny?: string[];        //예능 상
-        content?: string[];      //컨텐츠 상
-        original?: string[];     //원곡 재현 상
-        sleep?: string;          // 인기 상
-        unit?: string;           //유닛 상
-        new?: string;            //신인 상
-        grow?: string;           //성장 상
-        master?: string[];       //대 상
-        custom?: {              //내가 주고 싶은 상
-            episode: string;    // n회 m번째
-            content: string;    // 상 이름
-        }[];
-        message?: {             //메시지
-            name: string;       //받는 분
-            content: string;    //내용
-        }[];
-    }
+    problem?: number[];      //문제 10개
+    pitch?: string[];        //가창력 상
+    voice?: string[];        //음색 상
+    funny?: string[];        //예능 상
+    content?: string[];      //컨텐츠 상
+    original?: string[];     //원곡 재현 상
+    sleep?: string;          // 인기 상
+    unit?: string;           //유닛 상
+    new?: string;            //신인 상
+    grow?: string;           //성장 상
+    master?: string[];       //대 상
+    custom?: {              //내가 주고 싶은 상
+        episode: string;    // n회 m번째
+        content: string;    // 상 이름
+    }[];
+    message?: {             //메시지
+        name: string;       //받는 분
+        content: string;    //내용
+    }[];
 }
