b8ef7c37c6e56530ea880b953b82bf59597912f2
gcshift <gcshift@xinics.com>
2020-01-15 14:16:15 +0900

    VoteData 구조 변경

--- src/entities/VoteData.ts
+++ src/entities/VoteData.ts
@@ -1,18 +1,20 @@
+import { EpisodeData } from "./EpisodeData";
+
 export interface VoteData {
     email?: string;
     problem?: number[];      //문제 10개
-    pitch?: string[];        //가창력 상
-    voice?: string[];        //음색 상
-    funny?: string[];        //예능 상
-    content?: string[];      //컨텐츠 상
-    original?: string[];     //원곡 재현 상
-    sleep?: string;          // 인기 상
+    pitch?: EpisodeData[];        //가창력 상
+    voice?: EpisodeData[];        //음색 상
+    funny?: EpisodeData[];        //예능 상
+    content?: EpisodeData[];      //컨텐츠 상
+    original?: EpisodeData[];     //원곡 재현 상
+    sleep?: EpisodeData[];          // 인기 상
     unit?: string;           //유닛 상
     new?: string;            //신인 상
     grow?: string;           //성장 상
     master?: string[];       //대 상
     custom?: {              //내가 주고 싶은 상
-        episode: string;    // n회 m번째
+        episode: EpisodeData;    // n회 m번째
         content: string;    // 상 이름
     }[];
     message?: {             //메시지
