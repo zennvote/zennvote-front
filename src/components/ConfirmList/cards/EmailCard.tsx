43efdcf70da9380e335cda862c8d4972b1ae6dc5
gcshift <gcshift@xinics.com>
2020-01-15 14:02:07 +0900

    투표결과 EmailCard에 Divider 추가

--- src/components/ConfirmList/cards/EmailCard.tsx
+++ src/components/ConfirmList/cards/EmailCard.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import styles from "../styles";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Card, CardContent, Typography, Divider } from "@material-ui/core";
 import { VoteData } from "../../../entities/VoteData";
 
 interface EmailCardProps {
@@ -16,6 +16,7 @@ const EmailCard: FC<EmailCardProps> = ({ vote: { email } }) => {
           <Typography variant="h5" component="h2" className={classes.cardTitle}>
             <b>이메일</b>
           </Typography>
+          <Divider className={classes.divider} />
           <Typography className={classes.cardTypo}>
             { email }
           </Typography>
